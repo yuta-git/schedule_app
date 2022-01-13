a2d0e5f39596e9a8bccfb3e4ce4708c94d58c462
YutaIto <n88.ito.yuta@gmail.com>
2022-01-13 14:20:04 +0900

    welcome.blade.phpの変形

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 @section('title', 'Schedule App')
 @section('content')
+    <div class="row mt-3 mb-3">
+        <h1 class="col-sm-12 text-center">スケジュールアプリへようこそ！</h1>
+    </div>
     <div class="row">
         <a href="/signup" class="offset-sm-1 col-sm-4 btn btn-primary">新規会員登録</a>
         <a href="/login" class="offset-sm-1 col-sm-4 btn btn-danger">ログイン</a>
